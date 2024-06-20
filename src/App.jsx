5bda83593c1ca18cde540fa27d14d34ee9a4b4bb
Bishnu <thapabishnu20@gmail.com>
2024-06-20 12:27:17 +0545

    add first component & fragement

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,31 @@ function App() {
 
   return (
     <>
-      <img src='amo3.jpg' alt='annapurna circuit' height={250} width={250} />
-      <h2>Name:"Queen Of Tears"</h2>
-      <h3>Rating:4.8⭐</h3>
-      <p>Summary:Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt tempore nisi quaerat, dolores aut iste soluta veritatis exercitationem! Totam sunt, vero consequuntur ipsam illum amet quia quod est dolorem minus saepe quae obcaecati ut reiciendis veniam animi numquam repudiandae soluta tempora iusto similique magni reprehenderit velit deleniti. Quidem, pariatur eos.</p>
+      <NetFlixSerie />
+      <NetFlixSerie />
+      <NetFlixSerie />
     </>
   )
 }
+const NetFlixSerie = () => {
+  return (
+    <>
+      <img src="amo3.jpg" alt="annapurna circuit" height={250} width={250} />
+      <h2>Name:"Queen Of Tears"</h2>
+      <h3>Rating:4.8⭐</h3>
+      <p>
+        Summary:Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt
+        tempore nisi quaerat, dolores aut iste soluta veritatis exercitationem!
+        Totam sunt, vero consequuntur ipsam illum amet quia quod est dolorem
+        minus saepe quae obcaecati ut reiciendis veniam animi numquam
+        repudiandae soluta tempora iusto similique magni reprehenderit velit
+        deleniti. Quidem, pariatur eos.
+      </p>
+    </>
+  );
+  
+}
 
 export default App
+
+
