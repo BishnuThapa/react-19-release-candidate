9b021272b9a9158666d85ff57ea0f6d6fb258be6
Bishnu <thapabishnu20@gmail.com>
2024-06-20 15:52:59 +0545

    reusable card for netflix series

--- src/components/NetflixSeries.jsx
+++ src/components/NetflixSeries.jsx
@@ -1,30 +1,11 @@
 import seriesData from '../api/seriesData.json'
+import SeriesCard from './SeriesCard';
 
 const NetFlixSeries = () => {
     return (
         <ul>
             {seriesData.map((series) => {
-                return (
-                  <li key={series.id}>
-                    <div>
-                      <img
-                        src={series.img_url}
-                        alt={series.name}
-                        width="40%"
-                        height="40%"
-                      />
-                    </div>
-                    <h2>Name: {series.name}</h2>
-                    <h3>Rating: {series.rating}</h3>
-                    <p>Summery: {series.description}</p>
-                    <p>Genre: {series.genre}</p>
-                        <p>Cast: {series.cast}</p>
-                        <a href={series.watch_url} target='_blank'>
-                            <button>Watch Now</button>
-                        </a>
-                  </li>
-                ); 
-                
+                return <SeriesCard key={series.id} series={series} />;
             })}
       </ul>
   )
---
+++ src/components/SeriesCard.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const SeriesCard = ({series}) => {
+  return (
+    <li key={series.id}>
+      <div>
+        <img src={series.img_url} alt={series.name} width="40%" height="40%" />
+      </div>
+      <h2>Name: {series.name}</h2>
+      <h3>Rating: {series.rating}</h3>
+      <p>Summery: {series.description}</p>
+      <p>Genre: {series.genre}</p>
+      <p>Cast: {series.cast}</p>
+      <a href={series.watch_url} target="_blank">
+        <button>Watch Now</button>
+      </a>
+    </li>
+  ); 
+}
+
+export default SeriesCard
